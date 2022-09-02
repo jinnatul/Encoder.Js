29dde1cc5aa426fa954f8bb39fdb13a56b7ecacf
jinnatul <morolswediu@gmail.com>
2022-09-02 20:42:06 +0600

    feat: create code editor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { Col, Row, Input, Menu } from "antd";
+import React, { useState } from "react";
+import { Col, Row, Menu, Button } from "antd";
+import CodeEditor from "@uiw/react-textarea-code-editor";
+import "./Home.css";
 import {
   FolderAddOutlined,
   VerticalAlignBottomOutlined,
@@ -7,17 +9,22 @@ import {
   CopyOutlined,
   FullscreenOutlined,
   LinkOutlined,
+  FullscreenExitOutlined,
+  FormatPainterOutlined,
 } from "@ant-design/icons";
 
-const { TextArea } = Input;
 const { Item } = Menu;
 
 const Home = () => {
+  const [code, setCode] = useState(``);
+
+  console.log(code);
+
   return (
     <div>
       <Row>
-        <Col xs={{ span: 6, offset: 2 }} lg={{ span: 9, offset: 2 }}>
-          <Menu mode="horizontal">
+        <Col xs={{ span: 6, offset: 2 }} lg={{ span: 9, offset: 1 }}>
+          <Menu mode="horizontal" theme="dark">
             <Item key="file" icon={<FolderAddOutlined />}>
               Open File
             </Item>
@@ -34,10 +41,53 @@ const Home = () => {
               Clear
             </Item>
           </Menu>
-          <TextArea rows={20} />
+          <div className="App-editor">
+            <CodeEditor
+              value={code}
+              language="js"
+              placeholder="Write code here or paste code here"
+              minHeight={500}
+              onChange={(evn) => setCode(evn.target.value)}
+              style={{
+                fontSize: 14,
+                backgroundColor: "#f5f5f5",
+                fontFamily:
+                  "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+              }}
+            />
+          </div>
         </Col>
-        <Col xs={{ span: 5, offset: 2 }} lg={{ span: 9, offset: 2 }}>
-          <Menu mode="horizontal">
+        <Col xs={{ span: 2, offset: 1 }} lg={{ span: 2, offset: 1 }}>
+          <Row justify="center">
+            <Col>
+              <Button
+                type="primary"
+                shape="round"
+                icon={<FullscreenExitOutlined />}
+                size={"large"}
+                style={{ background: "#001529", borderColor: "green" }}
+              >
+                Minify
+              </Button>
+            </Col>
+          </Row>
+          <br></br>
+          <Row justify="center">
+            <Col>
+              <Button
+                type="primary"
+                shape="round"
+                icon={<FormatPainterOutlined />}
+                size={"large"}
+                style={{ background: "#001529", borderColor: "green" }}
+              >
+                Beautify
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+        <Col xs={{ span: 5, offset: 2 }} lg={{ span: 9, offset: 1 }}>
+          <Menu mode="horizontal" theme="dark">
             <Item key="save" icon={<VerticalAlignBottomOutlined />}>
               Save
             </Item>
@@ -51,7 +101,21 @@ const Home = () => {
               Clear
             </Item>
           </Menu>
-          <TextArea rows={20} />
+          <div className="App-editor">
+            <CodeEditor
+              value={code}
+              language="js"
+              placeholder="Output code here"
+              minHeight={500}
+              onChange={(evn) => setCode(evn.target.value)}
+              style={{
+                fontSize: 14,
+                backgroundColor: "#f5f5f5",
+                fontFamily:
+                  "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+              }}
+            />
+          </div>
         </Col>
       </Row>
     </div>
